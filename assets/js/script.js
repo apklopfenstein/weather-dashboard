b08be82e99da4be2685a18fba16af01c59345e36
Alyssa Klopfenstein <alyssa.r.purdy@gmail.com>
2020-09-06 15:37:43 -0500

    current weather finished

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 var cityInputEl = $('.form-control');
 var searchBtnEl = $('#search');
 var cityListEl = $('.list-group');
+var weatherContainerEl = $('#weather');
 var cities = JSON.parse(localStorage.getItem('cities')) || [];
 
 var searchClicked = function (event) {
@@ -21,15 +22,65 @@ var searchClicked = function (event) {
 };
 
 var getWeather = function (city) {
-    var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=edff0907543754128427ea37fa1d4a2d';
+    var appId = 'edff0907543754128427ea37fa1d4a2d';
+    var currentWeatherUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=imperial&appid=' + appId;
+    var currentUvUrl = 'http://api.openweathermap.org/data/2.5/uvi?appid=' + appId;
 
     // make requset to the url
-    return $.get(apiUrl)
+    return $.get(currentWeatherUrl)
         .then(function (data) {
-            // populate current weather
+            $.get(currentUvUrl + '&lat=' + data.coord.lat + '&lon=' + data.coord.lon)
+                .then(function (uvData) {
+                    weatherContainerEl.empty();
+                    console.log(uvData);
+                    // populate current weather
+                    createCurrentWeather(data, uvData);
+                })
 
             // make call to forecast API and populate forecast
         });
+};
+
+var createCurrentWeather = function (data, uvData) {
+    var cityInfoEl = $('<div class="city-info">');
+    weatherContainerEl.append(cityInfoEl);
+
+    var iconUrl = 'http://openweathermap.org/img/wn/' + data.weather[0].icon + '.png';
+    var cityNameEl = $('<h3>');
+    cityNameEl.text(data.name + ' (' + moment().format('L') + ') ');
+    var cityWeatherIconEl = $('<img src="' + iconUrl + '">');
+    cityNameEl.append(cityWeatherIconEl);
+    cityInfoEl.append(cityNameEl);
+
+    var tempEl = $('<p>');
+    tempEl.html('Temperature: ' + (Math.round(data.main.temp * 10) / 10) + ' &deg;F');
+    cityInfoEl.append(tempEl);
+
+    var humidityEl = $('<p>');
+    humidityEl.text('Humidity: ' + (data.main.humidity) + '%');
+    cityInfoEl.append(humidityEl);
+
+    var windSpeedEl = $('<p>');
+    windSpeedEl.text('Wind Speed: ' + (data.wind.speed) + ' MPH');
+    cityInfoEl.append(windSpeedEl);
+
+    var uvIndexEl = $('<p>');
+    var uvIndex = uvData.value;
+    uvIndexEl.text('UV Index: ');
+    var uvIndexValueEl = $('<span>');
+    uvIndexValueEl.text(uvIndex);
+    uvIndexEl.append(uvIndexValueEl);
+    cityInfoEl.append(uvIndexEl);
+
+    if (uvIndex <= 3) {
+        uvIndexValueEl.addClass('uv-low');
+    } else if (uvIndex <= 6) {
+        uvIndexValueEl.addClass('uv-med');
+    } else if (uvIndex <= 8) {
+        uvIndexValueEl.addClass('uv-med-high');
+    } else {
+        uvIndexValueEl.addClass('uv-high');
+    }
 };
 
 var createListItem = function (name) {
